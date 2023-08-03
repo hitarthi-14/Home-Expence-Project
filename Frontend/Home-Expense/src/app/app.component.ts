832dae5f2154b3fca4ab283bac4f1f94a23e7578
Hitarthi Rai <hitarthirai14@gmail.com>
2023-08-03 12:27:43 +0530

    Important Componants

--- Frontend/Home-Expense/src/app/app.component.ts
+++ Frontend/Home-Expense/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Home-Expense';
+
+  menuBottonStatus: boolean = true;
 }
--- Frontend/Home-Expense/src/app/app.module.ts
+++ Frontend/Home-Expense/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MonthsComponent } from './months/months.component';
+import { MonthComponent } from './month/month.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    MonthsComponent,
+    MonthComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
